77279537c67228ecf2f5394f62e7d60967bb683b
DeepakFromZeus <manedeep2001@gmail.com>
2024-11-09 18:35:38 +0530

    add filters in prtg list

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -42,7 +42,7 @@ const Header = () => {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to="/home" className={isLinkActive('/home')}>Home</Nav.Link>
                             <Nav.Link as={Link} to="/device-list" className={isLinkActive('/device-list')}>Device List</Nav.Link>
-                            <Nav.Link as={Link} to="/prtg" className={isLinkActive('/prtg')}>PRTG Map</Nav.Link>
+                            {/* <Nav.Link as={Link} to="/prtg" className={isLinkActive('/prtg')}>PRTG Map</Nav.Link> */}
                             <Nav.Link as={Link} to="/prtg-list" className={isLinkActive('/prtg-list')}>PRTG List</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
--- src/component/prtgList/index.js
+++ src/component/prtgList/index.js
@@ -1,16 +1,20 @@
 import { useMutation } from '@tanstack/react-query';
 import { useEffect, useState, useRef } from 'react';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Form, Table } from 'react-bootstrap';
 import { GetDeviceApi, GetPrtgList } from '../../api/deviceApi';
 
 const PrtgList = () => {
     const [devices, setDevices] = useState([]);
+    const [connectedCount, setConnectedCount] = useState(0);
+    const [type, setType] = useState("all");
+    const [filteredDeviceList, setFilteredDeviceList] = useState([]);
 
     const getMutation = useMutation({
         mutationFn: () => GetPrtgList(),
         onSuccess: (data) => {
-            console.log(data)
             setDevices(data.devices);
+            let count = data.devices.filter(d => d.status === 'Up');
+            setConnectedCount(count.length);
         },
         onError: (error) => {
             console.error("Api failed:", error.response?.data || error.message);
@@ -20,6 +24,17 @@ const PrtgList = () => {
         }
     });
 
+    const applyFilters = (devices) => {
+        let filteredList = devices.filter(d =>
+            type === "all" || d.status === type
+        );
+        setFilteredDeviceList(filteredList);
+    };
+
+    useEffect(() => {
+        applyFilters(devices);
+    }, [type]);
+
 
     useEffect(() => {
         getMutation.mutate();
@@ -28,6 +43,21 @@ const PrtgList = () => {
     return (
         <Container style={{ marginTop: '100px' }}>
             <h2>PRTG Device List</h2>
+            <div style={{ display: 'flex', gap: '10px', alignItems: 'center', marginBottom: '10px' }}>
+                <label style={{ color: 'white' }}>Filter : </label>
+                <Form.Select className="custom-select" style={{ width: '170px' }} aria-label="Filter" value={type} onChange={e => setType(e.target.value)}>
+                    <option value="all">All ({devices.length})</option>
+                    <option value="Up" style={{ color: 'green' }}>Connected ({connectedCount})</option>
+                    <option value="Paused" style={{ color: 'red' }}>Disconnected ({devices.length - connectedCount})</option>
+                </Form.Select>
+
+                {/* <label style={{ color: 'white' }}>Device Type : </label> */}
+                {/* <Form.Select style={{ width: '170px' }} aria-label="Filter" value={deviceType} onChange={e => setDeviceType(e.target.value)}>
+                    <option value="all">All ({devices.length})</option>
+                    <option value="ap">AP ({devices.filter(d => d.type === 'ap')?.length || 0})</option>
+                    <option value="switch">Switch ({devices.filter(d => d.type === 'switch')?.length || 0})</option>
+                </Form.Select> */}
+            </div>
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -40,14 +70,14 @@ const PrtgList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {devices?.map((device, index) => (
+                    {filteredDeviceList?.map((device, index) => (
                         <tr key={index}>
-                            <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.objid}</td>
+                            <td style={{ color: device.status === 'Up' ? 'green' : 'red' }}>{device.objid}</td>
                             {/* <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.probe}</td> */}
-                            <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.device}</td>
-                            <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.group}</td>
-                            <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.host}</td>
-                            <td style={{color: device.status === 'Up' ? 'green' : 'red'}}>{device.status === 'Up' ? 'Connected' : 'Disconnected'}</td>
+                            <td style={{ color: device.status === 'Up' ? 'green' : 'red' }}>{device.device}</td>
+                            <td style={{ color: device.status === 'Up' ? 'green' : 'red' }}>{device.group}</td>
+                            <td style={{ color: device.status === 'Up' ? 'green' : 'red' }}>{device.host}</td>
+                            <td style={{ color: device.status === 'Up' ? 'green' : 'red' }}>{device.status === 'Up' ? 'Connected' : 'Disconnected'}</td>
                         </tr>
                     ))}
                 </tbody>
